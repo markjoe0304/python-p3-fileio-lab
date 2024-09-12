e69381ee1afc85564c1f15c3e0b2496ade68c55c
markjoe0304 <markjoegicheru@gmail.com>
2024-09-12 20:41:41 +0300

    task done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    full_path = f"{file_name}.txt"
+    with open(full_path, "w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    full_path = f"{file_name}.txt"
+    with open(full_path, "a") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    full_path = f"{file_name}.txt"
+    with open(full_path, "r") as file:
+        return file.read()
